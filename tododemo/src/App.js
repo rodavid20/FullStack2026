895fdc1572fdee17de53018782f5a6ba89da6373
Roshan <rodavid20@users.noreply.github.com>
2026-02-04 13:25:07 +0530

    Update App.js

--- tododemo/src/App.js
+++ tododemo/src/App.js
@@ -5,17 +5,24 @@ function TodoHeader() {
 }
 
 function TodoForm({ handleAdd }) {
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+    handleAdd(text);
+    setText("");
+  };
   return (
-    <>
-      <input type="text" id="newTitle" />
-      <button
-        onClick={() => {
-          handleAdd(document.getElementById("newTitle").value);
-        }}
-      >
+    <form onSubmit={handleSubmit}>
+      <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      <button type="submit">
         Add New
       </button>
-    </>
+    </form>
   );
 }
 
